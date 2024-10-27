c8863ed4139285927b197fab7b79a0d1b45b2d4e
xfl12345 <xfl12345>
2024-10-27 15:07:45 +0800

    修复路径错误问题

--- compose/standalone/nginx/05-xfl-init.sh
+++ compose/standalone/nginx/05-xfl-init.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 ex="example"
 pv="private"
-base_path="/etc/nginx/snippets"
 
 # file_list="$(cat /usr/local/share/xfl/check_file_list.txt | grep -v '^$')"
-raw_file_list="$(cd $base_path/$ex && grep -r --include="*.conf" "include snippets/private" . )"
-generate_file_list="$(echo -e "$raw_file_list" | sed '/\*\.conf/d' | cut -d':' -f1 | sort | uniq)"
-mkdir_file_list="$(echo -e "$raw_file_list" | grep '*.conf;' | grep -oE 'include .*$' | sed 's#include ##g' | sed 's#\*\.conf;##g' | cut -d':' -f2 | sort | uniq)"
+raw_file_list="$(grep -r --include="*.conf" "include snippets/private" . /etc/nginx/nginx.conf)\n$(grep -r --include="*.conf" "include snippets/private" /etc/nginx/snippets/$ex)"
+generate_file_list="$(echo -e "$raw_file_list" | grep -v '*.conf;' | grep -oE 'include .*$' | sed 's#;$##' | cut -d' ' -f2 | sort | uniq)"
+mkdir_list="$(echo -e "$raw_file_list" | grep '*.conf;' | grep -oE 'include .*$' | sed 's#include ##g' | sed 's#\*\.conf;##g' | cut -d':' -f2 | sort | uniq)"
 
 just_log() {
     echo "[$(date '+%Y-%m-%d %H:%M:%S')][xfl_init] $1"
@@ -14,7 +13,7 @@ just_log() {
 
 just_log "Current ID[$(id)]"
 
-for rel_path in $mkdir_file_list; do
+for rel_path in $mkdir_list; do
     curr_private_dir_path="$(realpath -Pm /etc/nginx/${rel_path})"
     just_log "Checking dir[${curr_private_dir_path}]..."
     if [ ! -e $curr_private_dir_path ]; then
@@ -24,11 +23,11 @@ for rel_path in $mkdir_file_list; do
 done
 
 for rel_path in $generate_file_list; do
-    curr_example_file_path="$(realpath -Pm ${base_path}/${ex}/${rel_path})"
-    curr_private_file_path="$(realpath -Pm ${base_path}/${pv}/${rel_path})"
+    curr_private_file_path="$(realpath -Pm /etc/nginx/$rel_path)"
+    curr_example_file_path="$(echo $curr_private_file_path | sed 's#^/etc/nginx/snippets/private#/etc/nginx/snippets/example#')"
     just_log "Checking file[${curr_example_file_path}]..."
     if [ ! -e $curr_private_file_path -a -e $curr_example_file_path  ]; then
-        file_content="include ${curr_example_file_path};"
+        file_content="include $(echo $curr_example_file_path | sed 's#^/etc/nginx/##');"
         just_log "Generating file[${curr_private_file_path}] with content[${file_content}]..."
         just_log "${file_content}" >> $curr_private_file_path
     fi
