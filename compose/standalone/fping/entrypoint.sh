6dc304f6ea11283655288f547732b7f9caba91a1
xfl12345 <xfl12345>
2025-02-27 14:50:16 +0000

    perf(fping) 默认启动脚本减少 fping 浪费

--- compose/standalone/fping/entrypoint.sh
+++ compose/standalone/fping/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 [ -z "$PING_NET" ] && echo "ENV[PING_NET] is empty. Exiting with code 1..." && exit 1
 echo "ENV[PING_NET]=$PING_NET"
-fping -l -a -q -g $PING_NET 2>/dev/null | grep -v 'timed out'
+fping -l -a -q -g -A $PING_NET --ttl 5  2>/dev/null | grep -v 'timed out'
